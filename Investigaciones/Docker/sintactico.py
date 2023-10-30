cbf18f2a34df1f62db64ae3ee15abbf4b49d535f
Hector Garcia <hectorgarx2@gmail.com>
2023-10-29 19:30:07 -0600

    Practica Docker

---
+++ Investigaciones/Docker/sintactico.py
@@ -0,0 +1,14 @@
+def calcular(num1, num2):
+    print(num1 + num2)
+    print(num1 - num2)
+    print(num1 / num2)
+    print(num1 * num2)
+
+
+def run():
+    num1 = 15
+    num2 = 4
+    calcular(num1, num2)
+
+run()
+
